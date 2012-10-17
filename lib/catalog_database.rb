34d4bfd261f39ab3ee91d8f2a78c217bfccfb4db
jamesshieh <randomletters13@gmail.com>
2012-10-17 11:51:26 -0400

    removed unused files

--- lib/catalog_database.rb
+++
@@ -1,9 +0,0 @@
-class Catalog
-
-  # Create a catalog view that combines book and transactions
-
-  def self.get_catalog
-    @catalog = Book.includes(:transactions)
-    return @catalog
-  end
-end
